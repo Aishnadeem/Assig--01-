38bf72dbfeb029877dea7c437b71cc699d77e2a5
Aishnadeem <157931373+Aishnadeem@users.noreply.github.com>
2024-03-24 15:26:33 +0500

    Create project
    Create project

---
+++ index.js
@@ -0,0 +1,91 @@
+
+// Chapter 1 Q1
+alert("Error! please Enter a valid passward." );
+
+// Q2:
+alert("Welcom to js land... Happy codding!");
+
+// Q4:
+alert ("Welcom to js Land");
+
+alert ("Prevent this page from creating additional dialoags.");
+
+
+// Q4:
+alert ("Hello... i can run JS through my web browser's console");
+
+// ...X...
+
+// Chapter 2 Q1
+var userName = "John Doe"
+console.log(userName);
+
+// Q2
+
+var myName = "15 years old"
+console.log(myName);
+
+// Q3
+
+var massage = "Hello WORLD"
+alert(massage);
+
+
+// Q4
+
+var a = "Certificate Mobile application Development"
+alert(a);
+
+
+// Q5
+
+var b = "PIZZA PIZZ PIZ PI P"
+alert(b);
+
+// Q6
+
+var email = "My email address is example@example.com"
+alert(email);
+
+
+// Q7
+
+var book = "A smarter way to learn JavaScript"
+alert(book);
+
+
+// ...xxx...a.
+
+// chapter 3 
+
+// Q1
+var age = "15 years old"
+alert(age);
+
+// Q2
+var age = "My birthday year is 1990"
+document.write(age);
+
+var z = "John Doe is ordered 5 T-shirt(s) on XYZ Clothing store"
+document.write(z);
+
+// chapter 5
+
+var ali = 3
+console.log(ali + 5);
+
+var asad = 5
+console.log(asad * 5);
+
+var num = 55
+console.log(num / 5);
+
+var str = 60
+console.log(str - 30);
+
+var answer = 240
+console.log(answer % 20);
+
+
+var num1 = 5
+console.log(++num1)
